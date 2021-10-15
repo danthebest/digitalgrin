22808980627a6255e99c870508f8844a3536a4b8
Daniel Tossou <tossdaniel@gmail.com>
2021-10-15 11:27:22 +0000

    Renamed index to landing-page Created a new index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      colors: {
+        primary: '#101C3A',
+        secondary: '#2997EF'
+      },
       fontFamily: {
         body: ['Poppins']
       }
